b089ad760ee154b1f581bf01929307902cd2d12a
MIREGA LTDA. <app_mirega@mirega.cl>
2025-11-07 22:34:18 -0300

    Add files via upload

---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from './database.types';
+
+const supabaseUrl = import.meta.env.VITE_BOLT_DATABASE_URL || 'https://utpzjvmhqfgoehvwjuxa.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_BOLT_DATABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0cHpqdm1ocWZnb2VodndqdXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5ODg5NzEsImV4cCI6MjA3NzU2NDk3MX0.DXFy0xFNhp9QN9Xe6gIirkkfeqnhr5Uoh7CNFhzbGbw';
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
